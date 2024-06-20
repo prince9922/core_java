78aacf93d4537affd79b2d5bd59021b3704a2caf
Prince Kumar <princesingh531@gmail.com>
2024-06-20 21:06:20 +0530

    Add files via upload

---
+++ CommercialVehicle.java
@@ -0,0 +1,15 @@
+package com.inheritence;
+
+public class CommercialVehicle {
+	public void model(String model)
+	{
+		System.out.println("Model name for commercial vehicle is "+model);
+	}
+	
+	public void price(double price)
+	{
+		System.out.println("price for commercial is "+price);
+	}
+
+
+}
